216eb9d09c3a4704d13744de26eb8171db0c3ffc
vinita <vinita@gmail.com>
2023-11-02 00:24:00 +0530

    initial

--- InfoGate3/app/src/main/java/com/example/infogate3/MainActivity.java
+++ InfoGate3/app/src/main/java/com/example/infogate3/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.infogate3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, UserLogin.class));
+                finish();
+            }
+        },3000);
     }
 }
