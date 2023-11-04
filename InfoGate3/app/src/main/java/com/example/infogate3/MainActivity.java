2d4b925d71c101dfe7cdf51138ffd5b4d85dd22a
Prerana <prerana.rp2829@gmail.com>
2023-11-04 22:03:10 +0530

    ok

--- InfoGate3/app/src/main/java/com/example/infogate3/MainActivity.java
+++ InfoGate3/app/src/main/java/com/example/infogate3/MainActivity.java
@@ -14,12 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scanner);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, UserLogin.class));
+                startActivity(new Intent(MainActivity.this, Scanner.class));
                 finish();
             }
         },5000);
